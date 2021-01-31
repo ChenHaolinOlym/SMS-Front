3be549bc2c6475c0a28e720d8e73e67c00c3ea50
ChenHaolinOlym <1024792636@qq.com>
2021-01-31 21:22:11 +0800

    base layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <TopNav/>
+  <Content/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+<script setup lang="ts">
+import TopNav from './components/TopNav.vue'
+import Content from './components/Content.vue'
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,8 @@
+<template>
+  <p>TopNav</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
